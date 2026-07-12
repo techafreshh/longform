3b219ea44829b4ef3b2b2af7a3530a4154ff5294
Toluwanimi Leigh <issactoluwanimi@gmail.com>
2026-07-12 13:53:59 +0100

    feat(scene_gen): add Imagen 4.0 models to standard fallbacks

--- src/scene_gen.py
+++ src/scene_gen.py
@@ -85,6 +85,9 @@ def _generate_single_image(client, prompt: str, types, verbose: bool = True) -> 
 
     # 3. Add standard fallback models to ensure we try all potential options
     standard_fallbacks = [
+        "imagen-4.0-generate-001",
+        "imagen-4.0-ultra-generate-001",
+        "imagen-4.0-fast-generate-001",
         "imagen-3.0-generate-002",
         "imagen-3.0-generate-001",
         "imagen-3.0-fast-generate-001",
